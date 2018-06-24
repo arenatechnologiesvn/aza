59be261fdfb19f1003fcf67085b26b131d128dfd
ngocxuan123 <nnxuan09@gmail.com>
2018-06-24 11:22:52 +0700

    change dashboard

--- resources/assets/js/views/dashboard/BarChart.vue
+++ resources/assets/js/views/dashboard/BarChart.vue
@@ -7,7 +7,7 @@ import echarts from 'echarts'
 require('echarts/theme/macarons') // echarts theme
 import { debounce } from '~/utils'
 
-const animationDuration = 6000
+const animationDuration = 100
 
 export default {
   props: {
