6ebca80db5e3b301ab784a64f740203b2749a6f4
nguyenvansy <sy@dactech.vn>
2018-06-25 20:16:30 +0900

    modify eslint

--- resources/assets/js/api/product.js
+++ resources/assets/js/api/product.js
@@ -1,13 +1,13 @@
 import request from '~/utils/request';
 
-export function getProducts() {
+export function getProducts () {
   return request({
     url: '/api/products',
     method: 'get'
   });
 }
 
-export function storeProduct(params) {
+export function storeProduct (params) {
   return request({
     url: '/api/products',
     method: 'post',
@@ -15,9 +15,9 @@ export function storeProduct(params) {
   });
 }
 
-export function deleteProduct(productId) {
+export function deleteProduct (productId) {
   return request({
     url: `/api/products/${productId}`,
-    method: 'delete',
+    method: 'delete'
   });
 }
