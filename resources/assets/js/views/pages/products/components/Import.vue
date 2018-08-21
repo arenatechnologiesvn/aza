a77720be0c32c9402a53d519865af1d551299a6a
nguyenvansy <sy@dactech.vn>
2018-08-21 19:29:27 +0900

    update product import table

--- resources/assets/js/views/pages/products/components/Import.vue
+++ resources/assets/js/views/pages/products/components/Import.vue
@@ -10,35 +10,42 @@
         .table__wrapper
           .index__container
             .table
-              el-table(:data="tableData" border size="small" style="width: 100%; margin-top:20px;")
+              el-table(:data="showingData" border size="small" style="width: 100%; margin-top:20px;")
                 el-table-column(v-for="item of tableHeader" :prop="item" :label="item" :key="item")
-        //-         el-table-column(prop="id" label="TÁC VỤ" width="125" fixed="right")
-        //-           template(slot-scope="scope")
-        //-             el-tooltip(class="item" effect="dark" content="Sửa đổi" placement="top")
-        //-               el-button(icon="el-icon-edit" size="mini" round  @click="openEditPanel(scope.row.id)")
-        //-             el-tooltip(class="item" effect="dark" content="Xóa" placement="top")
-        //-               el-button(type="danger" icon="el-icon-delete" size="mini" round @click="deleteOneProduct(scope.row.id)")
-        //-     div.pagination__wrapper
-        //-       el-pagination(:current-page.sync="currentPage"
-        //-         :page-sizes="[10, 20, 30, 50]"
-        //-         :page-size="pageSize"
-        //-         layout="total, sizes, prev, pager, next"
-        //-         :total="totalDataNum"
-        //-         @size-change="sizeChange")
-        //- el-table(:data="tableData" border highlight-current-row style="width: 100%;margin-top:20px;")
-        //-   el-table-column(v-for="item of tableHeader" :prop="item" :label="item" :key="item")
+                el-table-column(v-if="tableData.length" label="TÁC VỤ" width="125" fixed="right")
+                  template(slot-scope="scope")
+                    el-tooltip(class="item" effect="dark" content="Xóa" placement="top")
+                      el-button(type="danger" icon="el-icon-delete" size="mini" round @click="removeProduct(scope.row)")
+            div.pagination__wrapper(v-if="tableData.length")
+              el-pagination(:current-page.sync="currentPage"
+                :page-sizes="[10, 20, 30, 50]"
+                :page-size="pageSize"
+                layout="total, sizes, prev, pager, next"
+                :total="totalDataNum"
+                @size-change="sizeChange")
 
 </template>tableHeader
 
 <script>
 import UploadExcelComponent from '~/components/UploadExcel/index.vue'
+
+const DEDAULT_PAGE_SIZE = 10
+
 export default {
   name: 'UploadExcel',
   components: { UploadExcelComponent },
+  computed: {
+    showingData() {
+      return this.extractData(this.tableData)
+    }
+  },
   data() {
     return {
       tableData: [],
-      tableHeader: []
+      tableHeader: [],
+      currentPage: 1,
+      totalDataNum: 0,
+      pageSize: DEDAULT_PAGE_SIZE,
     }
   },
   methods: {
@@ -56,6 +63,25 @@ export default {
     handleSuccess({ results, header }) {
       this.tableData = results
       this.tableHeader = header
+    },
+    extractData(data) {
+      this.totalDataNum = data.length;
+
+      const results = [];
+      const offset = (this.currentPage - 1) * this.pageSize;
+      data.forEach((item, index, array) => {
+        if (index < offset) return;
+        if (index >= offset + this.pageSize) return;
+        results.push(item);
+      });
+
+      return results;
+    },
+    sizeChange(newPageSize) {
+      this.pageSize = newPageSize;
+    },
+    removeProduct(code) {
+      console.log(code);
     }
   }
 }
